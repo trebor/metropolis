e23bdb1c79c9492b699ba7e28ac2e8a7f87aa7ee
Robert Harris <rharris@twitter.com>
2015-03-26 14:21:08 -0700

    basically working new layout

--- js/chart.js
+++ js/chart.js
@@ -1,8 +1,6 @@
-define(['d3', 'lodash', 'baseChart', 'heatMap', 'legend'], function(d3, _, BaseChart, HeatMap, Legend) {
-
 // base svg chart, which auto resizes to fit containing element
 
-var module = function($chartNode, customOptions, extendedEvents) {
+define(['d3', 'lodash', 'baseChart', 'heatMap', 'legend'], function(d3, _, BaseChart, HeatMap, Legend) {return function($chartNode, customOptions, extendedEvents) {
 
   var ROW_COUNT = 7;
   var COL_COUNT = 24;
@@ -11,13 +9,10 @@ var module = function($chartNode, customOptions, extendedEvents) {
   var LEGEND_WIDTH = LEGEND_HEIGHT * LEGEND_COUNT;
 
   var model = null;
-  var sensorMap = null;
-  var cities = null;
+  var sensors = null;
   var localEvents = [];
   var localOptions = {};
   var dimensions = null;
-  var legend = null;
-  var legendG = null;
   var width = null;
   var height = null;
   var svg = null;
@@ -29,6 +24,7 @@ var module = function($chartNode, customOptions, extendedEvents) {
 
   var margin = {top: 130, right: 10, bottom: 10, left: 10};
   var cityScale = d3.scale.ordinal();
+  var sensorScale = d3.scale.ordinal();
   var colorScale = d3.scale.category10();
   var offset = 0;
   var cityOrder = {
@@ -49,36 +45,9 @@ var module = function($chartNode, customOptions, extendedEvents) {
     svg = baseChart.getContainer()
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    legendG = svg.append('g').classed('legend', true);
-    legend = new Legend(legendG);
-
-    svg.append('text')
-      .classed('type-title', true)
-      .attr('text-anchor', 'middle')
-      .attr('y', margin.top / -2)
-      .attr('opacity', 0)
-      .attr('dy', '-.2em');
-
-    visualize();
   }
 
   function setData(data) {
-    sensorMap = SENSORS.map(function(sensor) {
-      return {
-        extent: d3.extent(data, function(d) {return d[sensor];}),
-        name: sensor
-      };
-    }).reduce(function(p, c) {
-      p[c.name] = {
-        extent: c.extent,
-        color: d3.scale.linear()
-          .range(['white', colorScale(c.name)])
-          .domain(c.extent)
-      };
-      return p;
-    }, {});
-
     var cityMap = d3.nest()
       .key(function(d) { return d.city_name; })
       .map(data);
@@ -87,83 +56,104 @@ var module = function($chartNode, customOptions, extendedEvents) {
       return cityOrder[a] - cityOrder[b];
     });
 
+    //cityNames = [cityNames[0]];
+
     cityScale.domain(cityNames);
+    sensorScale.domain(SENSORS);
 
-    cities = cityNames.map(function(name) {
-      var group = svg.append('g').classed(name, true);
-      var details = CITY_DETAILS[name];
-      var yFormat = function(d) {return details.format(new Date(d));};
+    sensors = SENSORS.map(function(sensor) {
 
-      group.append('text')
-        .classed('city-title', true).text(name)
-        .attr('dy', '.9em');
+      var extent = d3.extent(data, function(d) {return d[sensor];});
+      var color = d3.scale.linear().range(['white', colorScale(sensor)]).domain(extent);
+      var sensorGroup = svg.append('g').classed(sensor, true);
 
-      var hours = d3.range(COL_COUNT);
-      var xScale = d3.scale.ordinal().domain(hours);
-      var yScale = d3.scale.ordinal();
+      var cities = cityNames.map(function(name) {
+        var group = sensorGroup.append('g').classed(name, true);
+        var details = CITY_DETAILS[name];
+        var yFormat = function(d) {return details.format(new Date(d));};
 
-      var city = {
-        name: name,
-        group: group,
-        map: new HeatMap(group),
-        data: cityMap[name],
-        xAxis: d3.svg.axis().scale(xScale).orient('top' ).tickValues(hours),
-        yAxis: d3.svg.axis().scale(yScale).orient('left').tickPadding(10).tickFormat(yFormat)
+        group.append('text')
+          .classed('city-title', true).text(name)
+          .attr('dy', '.9em');
+
+        var hours = d3.range(COL_COUNT);
+        var xScale = d3.scale.ordinal().domain(hours);
+        var yScale = d3.scale.ordinal();
+
+        var city = {
+          name: name,
+          group: group,
+          map: new HeatMap(group),
+          data: cityMap[name],
+          xAxis: d3.svg.axis().scale(xScale).orient('top' ).tickValues(hours),
+          yAxis: d3.svg.axis().scale(yScale).orient('left').tickPadding(10).tickFormat(yFormat)
+        };
+
+        return city;
+      });
+
+      var sensorPack = {
+        name: sensor,
+        extent: extent,
+        color: color,
+        group: sensorGroup,
+        cities: cities
       };
 
-      return city;
+      setSensorTitle(sensorPack);
+      return sensorPack;
     });
-
-    visualize();
   }
 
-  function sensorName(sensor) {
+  function sensorTitle(sensor) {
     return SENSOR_DETAILS[sensor].title + '  ' + SENSOR_DETAILS[sensor].unit + '';
   }
 
-  function setTitle(title) {
-    if (d3.select('.type-title').text() != title) {
-      d3.select('.type-title')
-        .transition()
-        .attr('opacity', 0)
-        .transition()
-        .duration(0)
-        .text(title)
-        .transition()
-        .duration(TRANSITION_DURATION / 3)
-        .attr('opacity', 1);
-    }
-  }
+  function setSensorTitle(sensor) {
+    sensor.legendG = sensor.group.append('g')
+      .classed('legend', true);
 
-  function setLegand(color, extent) {
-    legend
-      .color(color)
-      .setData(extent, LEGEND_COUNT);
-  }
+    sensor.legend = new Legend(sensor.legendG)
+      .color(sensor.color)
+      .setData(sensor.extent, LEGEND_COUNT);
 
-  function setSensor(sensor) {
-    setTitle(sensorName(sensor));
-    setLegand(sensorMap[sensor].color, sensorMap[sensor].extent);
+    sensor.title = sensor.group.append('text')
+      .classed('type-title', true)
+      .attr('text-anchor', 'middle')
+      .attr('y', margin.top / -2)
+      .attr('opacity', 0)
+      .attr('dy', '-.2em');
+
+    sensor.title
+      .transition()
+      .attr('opacity', 0)
+      .transition()
+      .duration(0)
+      .text(sensorTitle(sensor.name))
+      .transition()
+      .duration(TRANSITION_DURATION / 3)
+      .attr('opacity', 1);
   }
 
   var timeFormat = d3.time.format('%H');
   var dateFormat = d3.time.format('%e %b');
 
   function setDate(sensor, date) {
-    setSensor(sensor);
-    cities.forEach(function(city) {
-      var yAxisValues = d3.range(ROW_COUNT).map(function(d, i) {
-        return date.getTime() + i * MS_INA_DAY;
-      });
-      city.yAxis.scale().domain(yAxisValues);
-      city.yAxis.tickValues(yAxisValues);
+    sensors.forEach(function(sensor){
+        sensor.cities.forEach(function(city) {
+          var yAxisValues = d3.range(ROW_COUNT).map(function(d, i) {
+            return date.getTime() + i * MS_INA_DAY;
+          });
+          city.yAxis.scale().domain(yAxisValues);
+          city.yAxis.tickValues(yAxisValues);
 
-      city.map
-        .color(function(d) {
-          var value = d[sensor];
-          return  value !== undefined && !isNaN(value) ? sensorMap[sensor].color(value) : '#666';
-        })
-        .setData(model.oneWeek(city.name, date), COL_COUNT, idAccess, city.xAxis, city.yAxis);
+          city.map
+            .color(function(d) {
+              var value = d[sensor.name];
+              return  value !== undefined && !isNaN(value) ? sensor.color(value) : '#666';
+            })
+            .setData(model.oneWeek(city.name, date), COL_COUNT, idAccess, city.xAxis, city.yAxis);
+        });
     });
 
     visualize();
@@ -179,28 +169,36 @@ var module = function($chartNode, customOptions, extendedEvents) {
     width = dimensions.width - (margin.left + margin.right);
     height = dimensions.height - (margin.top + margin.bottom);
     cityScale.rangeBands([0, height], 0.02, 0);
+    sensorScale.rangeBands([0, width], 0.02, 0);
     visualize();
   }
 
   function visualize() {
-    if (!svg || !cities) return;
+    if (!svg || !sensors) return;
 
-    svg
-      .select('text.type-title')
-      .attr('x', width / 2);
-
-    cities.forEach(function(city) {
-      city.group
+    sensors.forEach(function(sensor) {
+      sensor.group
         .transition()
         .duration(TRANSITION_DURATION)
-        .attr('transform', 'translate(0, ' + cityScale(city.name) + ')');
-      city.map.visualize(width, cityScale.rangeBand());
+        .attr('transform', 'translate(' + [sensorScale(sensor.name), 0] + ')');
+
+      sensor.cities.forEach(function(city) {
+        city.group
+          .transition()
+          .duration(TRANSITION_DURATION)
+          .attr('transform', 'translate(0, ' + cityScale(city.name) + ')');
+        city.map.visualize(sensorScale.rangeBand(), cityScale.rangeBand());
+      });
+
+      sensor.title
+        .attr('x', sensorScale.rangeBand() / 2);
+
+      sensor.legendG
+        .attr('transform', 'translate(' + [(sensorScale.rangeBand() - LEGEND_WIDTH) / 2, -LEGEND_HEIGHT + 0] + ')');
+
+      sensor.legend.visualize(LEGEND_WIDTH, LEGEND_HEIGHT);
     });
 
-    legendG
-      .attr('transform', 'translate(' + [(width - LEGEND_WIDTH) / 2, -LEGEND_HEIGHT + 0] + ')');
-
-    legend.visualize(LEGEND_WIDTH, LEGEND_HEIGHT);
   }
 
   function setModel(_model) {
@@ -217,9 +215,4 @@ var module = function($chartNode, customOptions, extendedEvents) {
 
   initialize(CITIES);
   return $.extend(exports, baseChart);
-};
-
-// end module
-
-  return module;
-});
+};});
